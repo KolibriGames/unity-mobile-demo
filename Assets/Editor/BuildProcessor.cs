39c80cb9917c6cf36960bf143c85c45b26ab05d4
Christian Saaler <christian.saaler@kolibrigames.com>
2020-06-16 12:47:33 +0200

    Added BuildProcessor to trigger Android build from CLI

---
+++ Assets/Editor/BuildProcessor.cs
@@ -0,0 +1,19 @@
+﻿﻿using UnityEditor;
+using UnityEngine.Windows;
+
+namespace Editor
+{
+    public static class BuildProcessor
+    {
+        [MenuItem("File/BuildProcessor/Android")]
+        public static void PerformBuild()
+        {
+            var scenes = new[] { "Assets/Scenes/SampleScene.unity" };
+
+            const string buildPath = "./Build/Android";
+            Directory.CreateDirectory(buildPath);
+
+            BuildPipeline.BuildPlayer(scenes, buildPath + "/demo.apk", BuildTarget.Android, BuildOptions.Development);
+        }
+    }
+}
