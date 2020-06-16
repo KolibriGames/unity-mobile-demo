0260c43971167053b8c8cc241cd174a5b08dfb35
Christian Saaler <christian.saaler@kolibrigames.com>
2020-06-16 10:21:51 +0200

    Added Unity demo project files

---
+++ Assets/Rotate.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    [SerializeField] private Transform _transform;
+    [SerializeField] [Range(0, 100)] private float _rotateFactor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var rotate = Time.deltaTime * _rotateFactor;
+        var localeRotation = _transform.localEulerAngles;
+        _transform.localEulerAngles =
+            new Vector3(localeRotation.x, localeRotation.y + rotate, localeRotation.z);
+    }
+}
